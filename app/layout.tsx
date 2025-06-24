583196aa0f2a97caef15c0ffc8a95b037c410400
Santos2175 <santoshgurung949@gmail.com>
2025-06-24 22:22:30 +0545

    App icon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,24 +5,17 @@ import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
 import Footer from '@/components/Home/Footer/Footer';
 import ScrollToTop from '@/components/Helper/ScrollToTop';
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 const font = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: 'Travel for you | next js 15',
+  title: 'Travel App',
   description: 'Travel landing page using next js 15',
+  icons: {
+    icon: '/travel-logo.png',
+  },
 };
 
 export default function RootLayout({
